310387a55fdbff8d43f9d19e802c916b61e58e94
Derick <120838145+derickandrade@users.noreply.github.com>
2025-09-13 17:48:04 -0300

    Update mutex-travessia-de-macacos.c

--- mutex-travessia-de-macacos.c
+++ mutex-travessia-de-macacos.c
@@ -8,9 +8,11 @@
 
 pthread_mutex_t lock_corda = PTHREAD_MUTEX_INITIALIZER; // Controla o acesso a corda, macacos na mesma direcao ou gorila
 pthread_mutex_t lock_turno = PTHREAD_MUTEX_INITIALIZER; // Garante que macacos de direcoes opostas nao entrem na corda ao mesmo tempo
-pthread_mutex_t lock_macacos = PTHREAD_COND_INITIALIZER; // Protege a variavel macacos_na_corda
+pthread_mutex_t lock_macacosAB = PTHREAD_MUTEX_INITIALIZER; // Protege a variavel macacos_na_corda
+pthread_mutex_t lock_macacosBA = PTHREAD_MUTEX_INITIALIZER;
 
-int macacos_na_corda = 0;
+int macacosAB = 0;
+int macacosBA = 0;
 
 void *macacoAB(void *arg);
 void *macacoBA(void *arg);
@@ -47,22 +49,24 @@ void *macacoAB(void *arg) {
     int id = *((int *) arg);
     while (1) {
         pthread_mutex_lock(&lock_turno); // Tem macaco indo de A para B nesse momento
-        pthread_mutex_lock(&lock_macacos); // Protege a variavel macacos_na_corda
-            macacos_na_corda++;
-            if (macacos_na_corda == 1) {
-                pthread_mutex_lock(&lock_corda); // Só macacos indo de A para B
+        pthread_mutex_lock(&lock_macacosAB); // Protege a variavel macacosAB
+            macacosAB++;            
+            if (macacosAB == 1) {
+                pthread_mutex_lock(&lock_corda); // O primeiro macaco AB indo locka a corda
             }
-        printf("Macaco %d indo de A para B\n", id);
-        pthread_mutex_unlock(&lock_macacos); 
+        pthread_mutex_unlock(&lock_macacosAB);                             
+        pthread_mutex_unlock(&lock_turno);                
+
+        printf("Macaco %d indo de A para B. Macacos indo de A para B: %d\n", id, macacosAB);                    
         sleep(1);
-        printf("Macaco %d chegou em B\n", id);
-        pthread_mutex_lock(&lock_macacos);
-            macacos_na_corda--;
-            if (macacos_na_corda == 0) {
+        pthread_mutex_lock(&lock_macacosAB);                   
+            macacosAB--;                      
+            if (macacosAB == 0) {
                 pthread_mutex_unlock(&lock_corda);
-            }
-        pthread_mutex_unlock(&lock_macacos);
-        pthread_mutex_unlock(&lock_turno);
+            }                                    
+        pthread_mutex_unlock(&lock_macacosAB);
+
+        printf("Macaco %d chegou em B. Macacos indo de A para B: %d\n", id, macacosAB);
     }
     pthread_exit(0);
 }
@@ -71,21 +75,25 @@ void *macacoBA(void *arg) {
     int id = *((int *) arg);
     while (1) {
         pthread_mutex_lock(&lock_turno);
-        pthread_mutex_lock(&lock_macacos);
-            macacos_na_corda++;
-            if (macacos_na_corda == 1) {
+        pthread_mutex_lock(&lock_macacosBA);
+            macacosBA++;
+            if (macacosBA == 1) {
                 pthread_mutex_lock(&lock_corda);
             }
-        printf("Macaco %d indo de B para A\n", id);
-        sleep(1);
-        printf("Macaco %d chegou em A\n", id);
-        pthread_mutex_lock(&lock_macacos);
-            macacos_na_corda--;
-            if (macacos_na_corda == 0) {
-                pthread_mutex_unlock(&lock_corda);
-            }
-        pthread_mutex_unlock(&lock_macacos);
+        pthread_mutex_unlock(&lock_macacosBA);                
         pthread_mutex_unlock(&lock_turno);
+        
+        printf("Macaco %d indo de B para A. Macacos indo de B para A: %d\n", id, macacosBA);
+        sleep(1);
+        pthread_mutex_lock(&lock_macacosBA);
+            macacosBA--;            
+            if (macacosBA == 0) {            
+                pthread_mutex_unlock(&lock_corda);                
+            }                        
+        pthread_mutex_unlock(&lock_macacosBA);
+        
+        printf("Macaco %d chegou em A. Macacos indo de B para A: %d\n", id, macacosBA);    
+        
     }
     pthread_exit(0);
 }
@@ -94,13 +102,15 @@ void *gorila(void *arg) {
     while (1) {
         pthread_mutex_lock(&lock_turno);
         pthread_mutex_lock(&lock_corda);
-        pthread_mutex_lock(&lock_macacos);
-        printf("Gorila atravessando a corda.\n");
-        sleep(3);
-        printf("Gorila chegou do outro lado.\n");
-        pthread_mutex_unlock(&lock_macacos);
-        pthread_mutex_unlock(&lock_corda);
+        
+        printf("Gorila atravessando a corda.\n");                                      
         pthread_mutex_unlock(&lock_turno);
+        sleep(rand() % 2 + 2);        
+        
+        printf("Gorila chegou do outro lado.\n");
+        
+        pthread_mutex_unlock(&lock_corda);
+        
     }
     pthread_exit(0);
 }
